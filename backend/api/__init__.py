f3b6bac2d7219ac48bc0cdacc22075a9d34b2672
Nguyễn  Đức Anh <nguyenducanh05032003@gmail.com>
2025-11-16 21:29:48 +0700

    refactor(api): Move url_prefix to blueprint definition

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 
-api_bp = Blueprint("api", __name__)
+api_bp = Blueprint("api", __name__, url_prefix="/api")
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_cors import CORS
 import os
 
 def create_app():
@@ -7,15 +6,13 @@ def create_app():
     static_dir = os.path.join(os.path.dirname(__file__), 'frontend', 'statics')
     
     app = Flask(__name__, template_folder=template_dir, static_folder=static_dir, static_url_path='/static')
-    
-    CORS(app)
 
     @app.route('/')
     def index():
         return render_template('index.html')
 
     from backend.api.routes import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(api_bp)
 
     return app
 
