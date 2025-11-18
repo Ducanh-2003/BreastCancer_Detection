11abcec689968034479876e129b387ce0f1f00be
Nguyễn  Đức Anh <nguyenducanh05032003@gmail.com>
2025-11-18 15:12:13 +0700

    feat: Add Log in prediction

--- backend/services/prediction.py
+++ backend/services/prediction.py
@@ -6,9 +6,15 @@ import base64
 def get_prediction(img_bytes):
     if model is None:
         raise ValueError("Model unloaded.")
-        
-    img = Image.open(io.BytesIO(img_bytes))
-    result = model(img)
+    
+    try:
+        img = Image.open(io.BytesIO(img_bytes))
+        print(f"Image mode: {img.mode}, size: {img.size}")
+        result = model(img)
+        print(f"Model inference result: {result}")
+    except Exception as e:
+        print(f"Error during prediction: {e}")
+        raise ValueError("Invalid image data.")
     
     img_with_boxes = result[0].plot()
     img_result = Image.fromarray(img_with_boxes[..., :: -1])
@@ -18,14 +24,18 @@ def get_prediction(img_bytes):
     img_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
     
     detections = []
+    
     for box in result[0].boxes:
-        class_id = int(box.cls[0])
         confidence = float(box.conf[0])
+        class_id = int(box.cls[0])
+        print(f"Detected {model.names[class_id]} with confidence {confidence}")
         detections.append({
             "class": model.names[class_id],
             "confidence": round(confidence, 2)
         })
-        
+    
+    
+    print(f"Resulting detections: {detections}")
     return {
         "image_base64": img_base64,
         "detections": detections
